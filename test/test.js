a2504ead1d3755f94ec041688938051d5daac2b2
Kunal Saini <kunalsainics@gmail.com>
2021-03-08 20:18:37 +0530

    initial commit

---
+++ test/test.js
@@ -0,0 +1,20 @@
+const test = require('ava');
+
+const Client = require('..');
+
+test.beforeEach((t) => {
+  const client = new Client({});
+  Object.assign(t.context, { client });
+});
+
+test('returns itself', (t) => {
+  t.true(t.context.client instanceof Client);
+});
+
+test('sets a config object', (t) => {
+  const client = new Client({
+    accessKey: 'test-key',
+    accessSecret:'test-secret'
+  });
+  t.true(client instanceof Client);
+});
